04e4b91eea09efb0c339d19507a78bb89ce8021e
AndreyWayne <kolosov@gilmon.com>
2018-08-14 14:58:29 +0500

    'span'

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -94,3 +94,27 @@ messaging.setBackgroundMessageHandler(function(payload) {
     return self.registration.showNotification(payload.data.title, payload.data);
     console.log(payload)
 });
+
+self.addEventListener('notificationclick', function(event) {
+    // извлекаем адрес перехода из параметров уведомления
+    const target = event.notification.data.click_action || '/';
+    event.notification.close();
+
+    // этот код должен проверять список открытых вкладок и переключатся на открытую
+    // вкладку с ссылкой если такая есть, иначе открывает новую вкладку
+    event.waitUntil(clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+    }).then(function(clientList) {
+        // clientList почему-то всегда пуст!?
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == target && 'focus' in client) {
+                return client.focus();
+            }
+        }
+
+        // Открываем новое окно
+        return clients.openWindow(target);
+    }));
+});
