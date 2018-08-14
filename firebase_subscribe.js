9dee430c48fadd1f907e8532d678a146ff3aa080
AndreyWayne <kolosov@gilmon.com>
2018-08-14 15:49:33 +0500

    's'

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -72,7 +72,6 @@ function setTokenSentToServer(currentToken) {
 
 messaging.onMessage(function(payload) {
     console.log('Message received. ', payload);
-    func()
     // регистрируем пустой ServiceWorker каждый раз
     navigator.serviceWorker.register('firebase-messaging-sw.js');
 
@@ -92,32 +91,3 @@ messaging.onMessage(function(payload) {
         }
     });
 });
-
-function func() {
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log(payload)
-    if (typeof payload.data.time != 'undefined') {
-        var time = new Date(payload.data.time * 1000);
-        var now = new Date();
-
-        if (time < now) { // истек срок годности уведомления
-            return null;
-        }
-
-        var diff = Math.round((time.getTime() - now.getTime()) / 1000);
-
-        // показываем реальное время в уведомлении
-        // будет сгенерировано сообщение вида: "Начало через 14 минут, в 21:00"
-        payload.data.body = 'Начало через ' +
-            Math.round(diff / 60) + ' минут, в ' + time.getHours() + ':' +
-            (time.getMinutes() > 9 ? time.getMinutes() : '0' + time.getMinutes())
-        ;
-    }
-
-    // Сохраяем data для получения пареметров в обработчике клика
-    payload.data.data = payload.data;
-
-    // Показываем уведомление
-    return self.registration.showNotification(payload.data.title, payload.data);
-});
-}
